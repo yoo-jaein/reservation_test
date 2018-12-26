72a29ff4aabe6d1f25cc8c29605142e01bbc3f70
Jaein Yoo <y_jaein@naver.com>
2018-12-27 01:41:45 +0900

    Update 20181118094235_create_posts.rb

--- db/migrate/20181118094235_create_posts.rb
+++ db/migrate/20181118094235_create_posts.rb
@@ -3,13 +3,13 @@ class CreatePosts < ActiveRecord::Migration[5.0]
     # 예약에 사용되는 모델 Post
     create_table :posts do |t|
       t.string :classroom   # 강의실 정보
-      t.text :starttime     # 대여 시작 시간
-      t.text :endtime       # 대여 종료 시간
+      t.text :starttime     # 예약 시작 시간
+      t.text :endtime       # 예약 종료 시간
       t.integer :user_id    # 예약 신청자의 id
       t.string :content     # 간단한 설명
       t.integer :people     # 사용 인원
-      t.datetime :date      # 처음엔 0, 만약 수락된다면 1이고 거절당하면 2의 상태.
-      t.integer :status 
+      t.datetime :date      # 예약 날짜
+      t.integer :status     # 처음엔 0, 만약 수락된다면 1이고 거절당하면 2의 상태.
       t.timestamps null: false
     end
   end
